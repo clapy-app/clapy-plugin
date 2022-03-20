ccae6adb31f62ddbe190f3ebe6f138808bdc1842
Antoine ORY-LAMBALLE <antoine.orylamballe@gmail.com>
2022-03-20 21:41:42 +0100

    WIP bug I don't understand

--- figma-plugin-clapy/src/backend/routes/5-export-code/serialize-node.ts
+++ figma-plugin-clapy/src/backend/routes/5-export-code/serialize-node.ts
@@ -1,3 +1,9 @@
 export function serializeNode() {
+  if (figma.currentPage.selection?.length !== 1) {
+    throw new Error('Selection is not exactly one node, which is not compatible with serialization.');
+  }
+  // const selectedNode = figma.currentPage.selection[0];
+  // const node = getParentCompNode(selectedNode).node || selectedNode;
+  // return { foo: node };
   return { foo: 2 };
 }
