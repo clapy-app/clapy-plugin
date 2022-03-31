0c9a8de2e8b5de4bda11540dbdfae0029572f80c
Antoine ORY-LAMBALLE <antoine.orylamballe@gmail.com>
2022-03-31 18:30:52 +0200

    Upgrade to React 18

--- figma-plugin-clapy/src/index.tsx
+++ figma-plugin-clapy/src/index.tsx
@@ -1,14 +1,17 @@
 import './core/theme/global.scss';
 
 import { StrictMode } from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { App } from './App';
 import { PreviewMode } from './core/dev-preview-mode/PreviewMode';
 import { store } from './core/redux/store';
 
-render(
+// Cast to any because typings don't recognize createRoot yet, despite the addition of src/typings/react-18.d.ts.
+const root = (ReactDOM as any).createRoot(document.getElementById('react-page'));
+
+root.render(
   <StrictMode>
     <Provider store={store}>
       {/* Initializes Apollo GraphQL client for child components */}
@@ -20,5 +23,4 @@ render(
       {/* </InitApolloProvider> */}
     </Provider>
   </StrictMode>,
-  document.getElementById('react-page'),
 );
