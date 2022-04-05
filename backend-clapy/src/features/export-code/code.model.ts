8eb77403146e6a620944cdf1efa43de806037d49
Antoine ORY-LAMBALLE <antoine.orylamballe@gmail.com>
2022-04-05 16:38:29 +0200

    Add support for web fonts

--- backend-clapy/src/features/export-code/code.model.ts
+++ backend-clapy/src/features/export-code/code.model.ts
@@ -14,6 +14,7 @@ export type JsxOneOrMore = ts.JsxChild | ts.JsxChild[];
 
 export interface ProjectContext {
   readonly compNamesAlreadyUsed: Set<string>;
+  readonly fontFamiliesUsed: Set<string>;
   readonly resources: CodeDict;
   readonly cssFiles: CodeDict;
 }
