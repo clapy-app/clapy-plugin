b46421c5d8022741a2b6169a9cadf6f9071d5638
Antoine ORY-LAMBALLE <antoine.orylamballe@gmail.com>
2022-04-23 00:38:16 +0200

    Fix text align with new default flex direction

--- figma-plugin-clapy/src/components-old/LoginHome/_ButtonBase_2/_ButtonBase_2.tsx
+++
@@ -1,9 +0,0 @@
-import classes from './_ButtonBase_2.module.css';
-
-export function _ButtonBase_2() {
-  return (
-    <div className={classes.root}>
-      <div className={classes.text}>Get Early Access</div>
-    </div>
-  );
-}
--- figma-plugin-clapy/src/components-old/LoginHome/_ButtonBase_3/_ButtonBase_3.tsx
+++
@@ -1,9 +0,0 @@
-import classes from './_ButtonBase_3.module.css';
-
-export function _ButtonBase_3() {
-  return (
-    <div className={classes.root}>
-      <div className={classes.text}>Don’t have an account yet? Sign up.</div>
-    </div>
-  );
-}
