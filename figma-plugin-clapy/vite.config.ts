7a8c333c87fdde6dcda16e9981c258fb78f87b20
Antoine ORY-LAMBALLE <antoine.orylamballe@gmail.com>
2022-05-29 16:36:22 +0200

    Tried vite-plugin-checker, not adopted (yet?)

--- figma-plugin-clapy/vite.config.ts
+++ figma-plugin-clapy/vite.config.ts
@@ -1,12 +1,18 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-import { addFrontDefineVarToContext, BuildContext, pluginDir } from './src/scripts/esbuild/build-prepare';
+// @ts-ignore
+import type { BuildContext } from './src/scripts/esbuild/build-prepare';
+import { addFrontDefineVarToContext, pluginDir } from './src/scripts/esbuild/build-prepare';
 
 const context: BuildContext = {};
 addFrontDefineVarToContext(context);
 export default defineConfig({
   root: `${pluginDir}/src`,
-  plugins: [react()],
+  // import checker from 'vite-plugin-checker';
+  // https://github.com/fi3ework/vite-plugin-checker
+  // Checker can be useful, but let's try with the IDE problems only.
+  // vite-plugin-checker to reinstall if we want to retry.
+  plugins: [react() /* , checker({ typescript: true }) */],
   define: context.defineEnvVar,
 });
