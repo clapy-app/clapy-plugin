c361d7fd95d47286370038f6b523c9ac151859e9
YacineKherbache <yacine@clapy.co>
2022-08-02 15:51:16 +0200

    fix problems with previous commit

--- backend-clapy/src/features/user/user.service.ts
+++ backend-clapy/src/features/user/user.service.ts
@@ -37,16 +37,23 @@ export class UserService {
     return { quotas: quotas, quotasMax: quotasMax, isLicenceExpired: isLicenceExpired };
   }
   async getQuotaCount(userId: string) {
+    const currentMonth = new Date().getMonth();
+    const currentYear = new Date().getFullYear();
+    const nextMonth = currentMonth === 11 ? 0 : currentMonth + 1;
     const csbSubQuery = this.generationHistoryRepository
       .createQueryBuilder('generationHistory')
       .select('generated_link')
       .distinctOn(['generationHistory.generated_link'])
-      .where({ auth0id: userId, generatedLink: Not('_zip') });
+      .where({ auth0id: userId, generatedLink: Not('_zip') })
+      .andWhere('generationHistory.created_at > :startDate', { startDate: new Date(currentYear, currentMonth, 1) })
+      .andWhere('generationHistory.created_at < :endDate', { endDate: new Date(currentYear, nextMonth + 1, 1) });
 
     const zipSubQuery = this.generationHistoryRepository
       .createQueryBuilder('generationHistory')
       .select('generated_link')
-      .where({ auth0id: userId, generatedLink: '_zip' });
+      .where({ auth0id: userId, generatedLink: '_zip' })
+      .andWhere('generationHistory.created_at > :startDate', { startDate: new Date(currentYear, currentMonth, 1) })
+      .andWhere('generationHistory.created_at < :endDate', { endDate: new Date(currentYear, nextMonth + 1, 1) });
 
     const genCountQuery = `select count(*) as count from (${csbSubQuery.getSql()} union all ${zipSubQuery.getSql()}) tmp`;
     const [{ count }] = await this.dataSource.query(genCountQuery, [userId, '_zip']);
