90bd94647f6af8592f4d591be1f1d26b00e6a4b6
Antoine ORY-LAMBALLE <antoine@clapy.co>
2022-08-08 15:08:33 +0200

    chore: create dev tools to enable dev-only features

--- figma-plugin-clapy/src/pages/3-Account/components/Container/Dropdown/Dropdown.tsx
+++ figma-plugin-clapy/src/pages/3-Account/components/Container/Dropdown/Dropdown.tsx
@@ -3,8 +3,10 @@ import Button from '@mui/material/Button';
 import type { FC } from 'react';
 import { memo, useCallback, useState } from 'react';
 import type React from 'react';
+import { useSelector } from 'react-redux';
 
 import { logout, refreshUser } from '../../../../../core/auth/auth-service.js';
+import { selectDevTools } from '../../../../../core/auth/auth-slice.js';
 import { env } from '../../../../../environment/env.js';
 import { useCallbackAsync2 } from '../../../../../front-utils/front-utils.js';
 import classes from './Dropdown.module.css';
@@ -17,6 +19,7 @@ interface Props {
   };
 }
 export const Dropdown: FC<Props> = memo(function Dropdown(props = {}) {
+  const devTools = useSelector(selectDevTools);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [loading, setLoading] = useState(false);
   const open = Boolean(anchorEl);
@@ -60,7 +63,7 @@ export const Dropdown: FC<Props> = memo(function Dropdown(props = {}) {
         }}
       >
         <MenuItem onClick={logoutBtn}>Logout</MenuItem>
-        {env.isDev && (
+        {(env.isDev || devTools) && (
           <MenuItem onClick={refreshSession} disabled={loading}>
             Refresh session (dev)
           </MenuItem>
