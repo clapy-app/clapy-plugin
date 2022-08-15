c521680f087d5724aeed1d2952b0cf4f97755c04
Antoine ORY-LAMBALLE <antoine@clapy.co>
2022-08-15 19:39:10 +0200

    Fix attempt: log front errors on the server

--- backend-clapy/src/app.controller.ts
+++ backend-clapy/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Headers, HttpCode, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Inject, Post, Req } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { decode } from 'jsonwebtoken';
 import type { Repository } from 'typeorm';
@@ -7,7 +7,6 @@ import { AppService } from './app.service.js';
 import { IsBrowserGet } from './auth/IsBrowserGet.decorator.js';
 import { LoginTokensEntity } from './auth/login-tokens.entity.js';
 import { PublicRoute } from './auth/public-route-annotation.js';
-import { handleException } from './core/unknown-exception.filter.js';
 import type { AccessTokenDecoded } from './features/user/user.utils.js';
 
 @PublicRoute()
@@ -36,8 +35,7 @@ export class AppController {
   }
 
   @Post('front-monitor')
-  @HttpCode(500)
-  frontReport(@Body() body: any, @Headers('Authorization') authHeader: string) {
+  frontReport(@Body() body: any, @Headers('Authorization') authHeader: string, @Req() req: Request) {
     if (!body) body = {};
     let { message, stack } = body;
     if (!message) message = 'Unknown error';
@@ -53,8 +51,9 @@ export class AppController {
     // Extract user data from the header. In public routes, express-jwt is not run so the user is not in the request.
     const accessToken = authHeader?.length >= 8 ? authHeader.slice(7) : authHeader;
     const user = accessToken ? (decode(accessToken) as AccessTokenDecoded) : undefined;
+    // TODO to update when upgrading express-jwt that changes the key from `user` to `auth`. It will provide a new interface for that, that we should use, so that the code becomes strongly typed.
+    (req as any).user = user;
 
-    handleException(err, null, user);
-    return { ok: true };
+    throw err;
   }
 }
