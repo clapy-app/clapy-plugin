635bd011842b6ecb2995bca6c5fa70f2c6e09959
Antoine ORY-LAMBALLE <antoine@clapy.co>
2022-08-16 15:34:28 +0200

    Comment useless files requiring a dependency

--- backend-clapy/src/scripts/details/hasura-schema.utils.ts
+++ backend-clapy/src/scripts/details/hasura-schema.utils.ts
@@ -1,132 +1,133 @@
-import { Logger } from '@nestjs/common';
-import axios from 'axios';
-
-import { wait } from '../../common/general-utils.js';
-import { env } from '../../env-and-config/env.js';
-import { backendDir, pluginDir } from '../../root.js';
-
-// Require dev dependencies:
-// mkdirp
-// graphql-fetch-schema
-// @graphql-codegen/cli
-// @graphql-codegen/typescript
-
-const rootHasuraUrl = `${env.hasuraHttp}/v1`;
-
-const secret = env.hasuraAdminSecret;
-const frontGeneratedDir = `${pluginDir}/generated`;
-const apiGeneratedDir = `${backendDir}/generated`;
-const logger = new Logger('hasura-schema');
-
-export async function hasuraReloadMetadataAndSchema() {
-  logger.log('Reload Hasura metadata...');
-  await withRetry(hasuraReloadMetadata);
-  // Uncomment if we have remote schema. They should be refreshed as well.
-  // await withRetry(hasuraReloadRemoteSchema);
-  logger.log('Metadata refreshed.');
-}
-
-export async function generateTypes() {
-  if (!env.isDev) {
-    throw new Error('GraphQL types should only be generated in dev environment.');
-  }
-  logger.log('Extract Hasura schema and types for front...');
-  await createGeneratedFolder();
-  await hasuraExtractSchema();
-  await generateTypesFromSchema();
-  logger.log('Schema and types extracted.');
-}
-
-const retryCodes = ['ECONNREFUSED', 'ECONNRESET'];
-const retryDelay = 3;
-
-async function withRetry<T>(func: (...args: any[]) => Promise<T>): Promise<T> {
-  try {
-    return await func();
-  } catch (e: any) {
-    const connectionFailed = retryCodes.includes(e?.code);
-    const inconsistentMetadata =
-      e?.response?.data?.error === 'cannot continue due to newly found inconsistent metadata';
-    if (connectionFailed || inconsistentMetadata) {
-      if (connectionFailed) {
-        logger.error(`Couldn't connect to Hasura. Is Hasura container started? Will retry in ${retryDelay} seconds...`);
-      } else if (inconsistentMetadata) {
-        logger.error(`Couldn't connect to Hasura, inconsistent metadata. Will retry in ${retryDelay} seconds...`);
-      }
-      await wait(retryDelay * 1000);
-      return await withRetry(func);
-    } else {
-      // tslint:disable-next-line:no-console
-      console.warn('Failed with code:', e.code);
-      throw e;
-    }
-  }
-}
-
-async function hasuraReloadMetadata() {
-  return axios.post(
-    `${rootHasuraUrl}/query`,
-    {
-      type: 'reload_metadata',
-      args: {},
-    },
-    {
-      headers: {
-        'X-Hasura-Role': 'admin',
-        'X-Hasura-Admin-Secret': secret,
-      },
-    },
-  );
-}
-
-async function createGeneratedFolder() {
-  const mkdirp = (await import('mkdirp')).default;
-  return Promise.all([mkdirp(frontGeneratedDir), mkdirp(apiGeneratedDir)]);
-}
-
-async function hasuraExtractSchema() {
-  const fetchSchemaImport = await import('graphql-fetch-schema');
-  let fetchSchema = fetchSchemaImport.default;
-  if (fetchSchema.default) fetchSchema = fetchSchema.default;
-  return Promise.all([
-    fetchSchema(`${rootHasuraUrl}/graphql`, {
-      json: false,
-      graphql: true,
-      outputPath: frontGeneratedDir,
-      headers: {
-        'X-Hasura-Role': 'admin',
-        'X-Hasura-Admin-Secret': secret,
-      },
-    }),
-    fetchSchema(`${rootHasuraUrl}/graphql`, {
-      json: false,
-      graphql: true,
-      outputPath: apiGeneratedDir,
-      headers: {
-        'X-Hasura-Role': 'admin',
-        'X-Hasura-Admin-Secret': secret,
-      },
-    }),
-  ]);
-}
-
-async function generateTypesFromSchema() {
-  const { generate } = await import('@graphql-codegen/cli');
-  return generate(
-    {
-      schema: `${frontGeneratedDir}/schema.graphql`,
-      // documents: './src/**/*.graphql',
-      emitLegacyCommonJSImports: false,
-      generates: {
-        [`${frontGeneratedDir}/schema.d.ts`]: {
-          plugins: ['typescript'],
-        },
-        [`${apiGeneratedDir}/schema.d.ts`]: {
-          plugins: ['typescript'],
-        },
-      },
-      silent: true,
-    },
-    true,
-  );
-}
+export {};
+// import { Logger } from '@nestjs/common';
+// import axios from 'axios';
+// 
+// import { wait } from '../../common/general-utils.js';
+// import { env } from '../../env-and-config/env.js';
+// import { backendDir, pluginDir } from '../../root.js';
+// 
+// // Require dev dependencies:
+// // mkdirp
+// // graphql-fetch-schema
+// // @graphql-codegen/cli
+// // @graphql-codegen/typescript
+// 
+// const rootHasuraUrl = `${env.hasuraHttp}/v1`;
+// 
+// const secret = env.hasuraAdminSecret;
+// const frontGeneratedDir = `${pluginDir}/generated`;
+// const apiGeneratedDir = `${backendDir}/generated`;
+// const logger = new Logger('hasura-schema');
+// 
+// export async function hasuraReloadMetadataAndSchema() {
+//   logger.log('Reload Hasura metadata...');
+//   await withRetry(hasuraReloadMetadata);
+//   // Uncomment if we have remote schema. They should be refreshed as well.
+//   // await withRetry(hasuraReloadRemoteSchema);
+//   logger.log('Metadata refreshed.');
+// }
+// 
+// export async function generateTypes() {
+//   if (!env.isDev) {
+//     throw new Error('GraphQL types should only be generated in dev environment.');
+//   }
+//   logger.log('Extract Hasura schema and types for front...');
+//   await createGeneratedFolder();
+//   await hasuraExtractSchema();
+//   await generateTypesFromSchema();
+//   logger.log('Schema and types extracted.');
+// }
+// 
+// const retryCodes = ['ECONNREFUSED', 'ECONNRESET'];
+// const retryDelay = 3;
+// 
+// async function withRetry<T>(func: (...args: any[]) => Promise<T>): Promise<T> {
+//   try {
+//     return await func();
+//   } catch (e: any) {
+//     const connectionFailed = retryCodes.includes(e?.code);
+//     const inconsistentMetadata =
+//       e?.response?.data?.error === 'cannot continue due to newly found inconsistent metadata';
+//     if (connectionFailed || inconsistentMetadata) {
+//       if (connectionFailed) {
+//         logger.error(`Couldn't connect to Hasura. Is Hasura container started? Will retry in ${retryDelay} seconds...`);
+//       } else if (inconsistentMetadata) {
+//         logger.error(`Couldn't connect to Hasura, inconsistent metadata. Will retry in ${retryDelay} seconds...`);
+//       }
+//       await wait(retryDelay * 1000);
+//       return await withRetry(func);
+//     } else {
+//       // tslint:disable-next-line:no-console
+//       console.warn('Failed with code:', e.code);
+//       throw e;
+//     }
+//   }
+// }
+// 
+// async function hasuraReloadMetadata() {
+//   return axios.post(
+//     `${rootHasuraUrl}/query`,
+//     {
+//       type: 'reload_metadata',
+//       args: {},
+//     },
+//     {
+//       headers: {
+//         'X-Hasura-Role': 'admin',
+//         'X-Hasura-Admin-Secret': secret,
+//       },
+//     },
+//   );
+// }
+// 
+// async function createGeneratedFolder() {
+//   const mkdirp = (await import('mkdirp')).default;
+//   return Promise.all([mkdirp(frontGeneratedDir), mkdirp(apiGeneratedDir)]);
+// }
+// 
+// async function hasuraExtractSchema() {
+//   const fetchSchemaImport = await import('graphql-fetch-schema');
+//   let fetchSchema = fetchSchemaImport.default;
+//   if (fetchSchema.default) fetchSchema = fetchSchema.default;
+//   return Promise.all([
+//     fetchSchema(`${rootHasuraUrl}/graphql`, {
+//       json: false,
+//       graphql: true,
+//       outputPath: frontGeneratedDir,
+//       headers: {
+//         'X-Hasura-Role': 'admin',
+//         'X-Hasura-Admin-Secret': secret,
+//       },
+//     }),
+//     fetchSchema(`${rootHasuraUrl}/graphql`, {
+//       json: false,
+//       graphql: true,
+//       outputPath: apiGeneratedDir,
+//       headers: {
+//         'X-Hasura-Role': 'admin',
+//         'X-Hasura-Admin-Secret': secret,
+//       },
+//     }),
+//   ]);
+// }
+// 
+// async function generateTypesFromSchema() {
+//   const { generate } = await import('@graphql-codegen/cli');
+//   return generate(
+//     {
+//       schema: `${frontGeneratedDir}/schema.graphql`,
+//       // documents: './src/**/*.graphql',
+//       emitLegacyCommonJSImports: false,
+//       generates: {
+//         [`${frontGeneratedDir}/schema.d.ts`]: {
+//           plugins: ['typescript'],
+//         },
+//         [`${apiGeneratedDir}/schema.d.ts`]: {
+//           plugins: ['typescript'],
+//         },
+//       },
+//       silent: true,
+//     },
+//     true,
+//   );
+// }
--- backend-clapy/src/scripts/gentypes.ts
+++ backend-clapy/src/scripts/gentypes.ts
@@ -1,22 +1,23 @@
-import { generateTypes } from './details/hasura-schema.utils.js';
-
-// Deprecated script. To remove once we confirm we just need the CLI to generate the schemas.
-// After that, also check the hasura-schema utils. They may remain somewhere if needed later, e.g. another project.
-// But if not required in this project, we can clean it up.
-
-// Run it from the host machine, not inside docker. This script needs to access the backend and plugin project directories.
-(async () => {
-  try {
-    // TODO should use graphql-cli instead and watch schema changes to generate the typings.
-    // https://stackoverflow.com/a/42010467/4053349
-    // Ideally, generating types should happen directly on each change on Hasura, especially
-    // if we use the same DB and hasura instance for multiple projects.
-    // For now, let's say each project is responsible for generating its types.
-
-    // Alternatively, I can have a look at Apollo doc, codegen section:
-    // https://www.apollographql.com/docs/react/development-testing/developer-tooling/
-    await generateTypes();
-  } catch (error) {
-    console.error(error);
-  }
-})();
+export {};
+// import { generateTypes } from './details/hasura-schema.utils.js';
+// 
+// // Deprecated script. To remove once we confirm we just need the CLI to generate the schemas.
+// // After that, also check the hasura-schema utils. They may remain somewhere if needed later, e.g. another project.
+// // But if not required in this project, we can clean it up.
+// 
+// // Run it from the host machine, not inside docker. This script needs to access the backend and plugin project directories.
+// (async () => {
+//   try {
+//     // TODO should use graphql-cli instead and watch schema changes to generate the typings.
+//     // https://stackoverflow.com/a/42010467/4053349
+//     // Ideally, generating types should happen directly on each change on Hasura, especially
+//     // if we use the same DB and hasura instance for multiple projects.
+//     // For now, let's say each project is responsible for generating its types.
+// 
+//     // Alternatively, I can have a look at Apollo doc, codegen section:
+//     // https://www.apollographql.com/docs/react/development-testing/developer-tooling/
+//     await generateTypes();
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
