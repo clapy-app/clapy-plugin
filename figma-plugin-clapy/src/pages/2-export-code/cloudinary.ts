ba294f045387aeb6b3e3811f568844551c1e7285
Antoine ORY-LAMBALLE <antoine@clapy.co>
2022-08-24 15:39:33 +0200

    Fix: better error when an image is too big

--- figma-plugin-clapy/src/pages/2-export-code/cloudinary.ts
+++ figma-plugin-clapy/src/pages/2-export-code/cloudinary.ts
@@ -22,7 +22,13 @@ async function uploadAsset(file: Blob | string, imageHash: string) {
   const resp = await (await fetch(cloudinaryUploadUrl, { method: 'POST', body: formData })).json();
   const { secure_url } = resp;
   if (!secure_url) {
-    throw new Error(`BUG Missing cloudinary response URL. Response: ${JSON.stringify(resp)}`);
+    throw new Error(
+      resp?.error?.message
+        ? resp.error.message.includes?.('File size too large')
+          ? `One of the images in your design exceeds 10 MB, which is not currently supported. Please try to compress them and retry. Please let us know if this is not an option.`
+          : `${resp?.error?.message} (CDN response for images)`
+        : `BUG Missing cloudinary response URL. Response: ${JSON.stringify(resp)}`,
+    );
   }
   return secure_url as string;
 }
